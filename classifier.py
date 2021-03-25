f26f91d5bd84b6c380dcb26920218db7ec6b7ead
danielphilippi <daniel2.philippi@gmail.com>
2021-03-25 11:30:36 +0100

    some code cleaning and todos from meeting

--- classifier.py
+++ classifier.py
@@ -4,19 +4,22 @@ import os
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 from keras.layers.convolutional import UpSampling2D, Conv2D, MaxPooling2D
-from keras.layers import Input, Dense, Reshape, Flatten, Embedding, multiply, Dropout, Activation, BatchNormalization, ZeroPadding2D
+from keras.layers import Input, Dense, Reshape, Flatten, Embedding, multiply, Dropout, Activation, BatchNormalization,\
+    ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report, confusion_matrix
 from data_manager import load_dataset
 
 
-class Classifier():
+# Todo move to models.py
+class Classifier:
     def __init__(self, classifier, optimizer, input_shape=(28, 28, 1), num_cat=11):
         self.input_shape = input_shape
         self.num_cat = num_cat
+        self.num_img = None
         self.optimizer = optimizer
-
+        self.history = None
         self.model = self.build_classifier(classifier)
 
         self.model.compile(
@@ -24,18 +27,23 @@ class Classifier():
             loss='sparse_categorical_crossentropy'
         )
 
+        self.conf_mat = None
+        self.report = None
+
+    # Todo move definition to mods.classifier and pass to the class
     def build_classifier(self, classifier):
         cnn_model = Sequential()
 
-        cnn_model.add(Conv2D(32, (3,3), activation = 'relu',
-                       input_shape=self.input_shape))
-        cnn_model.add(MaxPooling2D((2,2)))
+        cnn_model.add(
+            Conv2D(32, (3, 3), activation='relu',
+                   input_shape=self.input_shape))
+        cnn_model.add(MaxPooling2D((2, 2)))
         cnn_model.add(Conv2D(64, (3, 3), activation = 'relu'))
-        cnn_model.add(MaxPooling2D((2,2)))
+        cnn_model.add(MaxPooling2D((2, 2)))
         cnn_model.add(Conv2D(128, (3, 3), activation = 'relu'))
-        cnn_model.add(MaxPooling2D((2,2)))
+        cnn_model.add(MaxPooling2D((2, 2)))
         cnn_model.add(Conv2D(128, (3, 3), activation = 'relu'))
-        cnn_model.add(MaxPooling2D((2,2)))
+        cnn_model.add(MaxPooling2D((2, 2)))
         
         cnn_model.add(Flatten())
         
@@ -48,37 +56,43 @@ class Classifier():
 
         return cnn_model
 
+    @staticmethod
+    # Todo: move to data_manager module
     def preprocess(self, train_dir, validation_dir):
         # rescale all images by 1/255
         train_datagen = ImageDataGenerator(rescale=1./255)
         test_datagen = ImageDataGenerator(rescale=1./255)
 
+        # Todo(DP): limit number of imgs passed from the dir to the generator
+        # Todo (DP): train / test split inside ImageDataGenerator
+        # Todo (DP): 
         train_set = train_datagen.flow_from_directory(
                                 train_dir, # target directory
-                                target_size = (150,150), #resizes all images by 150 x 150
+                                target_size = (28, 28),
                                 batch_size = 20,
-                                class_mode='binary') # because you use binary_crossentropy, need binary labels
+                                class_mode='categorical')
     
         test_set = test_datagen.flow_from_directory(
                                 validation_dir, 
-                                target_size = (150,150), 
+                                target_size = (28, 28),
                                 batch_size = 20,
-                                class_mode='binary')
+                                class_mode='categorical')
 
         train_set.class_indices
 
         return train_set, test_set
-    
+
+    # Todo: make args part of self
     def train(self, train_set, test_set):
-        
-        history = self.model.fit(train_set, steps_per_epoch=100,
-        epochs=30,
-        validation_data=test_set,
-        validation_steps=50)
+        self.num_img = train_set.shape[0]
+        self.history = self.model.fit(
+            train_set,
+            steps_per_epoch=100,
+            epochs=30,
+            validation_data=test_set,
+            validation_steps=50
+        )
 
-        return history
-
-    
     def save_model(self):
 
         def save(model, model_name):
@@ -92,22 +106,23 @@ class Classifier():
 
         save(self.model, "classifier")
 
-        
-    def plot_model(self, history):
+    # Todo: move to helper fcts module
+    def plot_model(self):
 
-        acc = history.history['acc']
-        val_acc = history.history['val_acc']
-        loss = history.history['loss']
-        val_loss = history.history['val_loss']
+        acc = self.history['acc']
+        val_acc = self.history['val_acc']
+        loss = self.history['loss']
+        val_loss = self.history['val_loss']
 
         epochs = range(1, len(acc) + 1)
+
+        # Todo: generate instance fig and return fig
+        fig = plt.figure()
 
         plt.plot(epochs, acc, 'bo', label = 'Training acc')
         plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
         plt.title('Training and validation accuracy')
         plt.legend()
-
-        plt.figure()
 
         plt.plot(epochs, loss, 'bo', label = 'Training loss')
         plt.plot(epochs, val_loss, 'b', label = 'Validation loss')
@@ -116,18 +131,19 @@ class Classifier():
 
         plt.show()
 
-    def metrics(self, test_set):
+    def evaluate(self, test_set):
         # if you have the last version of tensorflow, the predict_generator is deprecated.
         # you should use the predict method.
         # if you do not have the last version, you must use predict_generator
-        Y_pred = self.model.predict(test_set, 63) # ceil(num_of_test_samples / batch_size)
-        Y_pred = (Y_pred>0.5)
+        y_pred = self.model.predict(test_set, 63) # ceil(num_of_test_samples / batch_size)
+        y_pred = (y_pred>0.5)
         print('Confusion Matrix')
-        print(confusion_matrix(test_set.classes, Y_pred))
+        self.conf_mat = confusion_matrix(test_set.classes, y_pred)
+        print(self.conf_mat)
         print('Classification Report')
         target_names = ['Cats', 'Dogs']
-        print(classification_report(test_set.classes, Y_pred, target_names=target_names))
+        self.report = classification_report(test_set.classes, y_pred, target_names=target_names)
+        print(self.report)
 
-    # TODO
-    def report(self):
-        pass
+
+
